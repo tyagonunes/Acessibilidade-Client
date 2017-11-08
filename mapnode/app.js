e5ebb89106c061c21dadcf29b6ccdbb8e2748d7a
Tyago Nunes <tyagonunes@gmail.com>
2017-11-08 08:52:37 -0300

    novo modulo

--- mapnode/app.js
+++ mapnode/app.js
@@ -1,51 +1,10 @@
 var app = require('./config/app_config');
-
 var db =  require('./config/db_config');
-
 var Local = require('./models/local');
-
 var localController = require('./controllers/localController');
 
-// var express = require('express');
-
-// var app = express();
-
-// var bodyParser = require('body-parser');
-
-// var db_string = 'mongodb://127.0.0.1/mapnode';
-
-// var mongoose = require('mongoose').connect(db_string);
-
-// var db = mongoose.connection;
-
-// db.on('error', console.error.bind(console, "Erro ao conectar no banco"));
-// db.once('open', function(){
-// 	var localSchema = mongoose.Schema({
-// 		nome: String,
-// 		latitude: Number,
-// 		longitude: Number,
-// 		tipo: String,
-// 		descricao: String,
-// 		acessos: [{type: String}]
-
-// 	});
-// 	Local = mongoose.model('Local', localSchema);
-// });
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(function(req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type, Authorization");
-//   next();
-// });
-
-// app.listen(5000);
 
 // LISTAR LOCAIS
-
 app.get('/', function(req, res) {
 	res.end('Bem vindo a API');	
 });
@@ -53,11 +12,9 @@ app.get('/', function(req, res) {
 
 
 app.get('/locais', function(req, res) {
-
 	localController.list(function(resp){
 		res.json(resp);
 	});
-
 });
 
 
